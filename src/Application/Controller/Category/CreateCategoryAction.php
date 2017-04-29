1cc5436c0d077c96ec853d526303cbf8b0df1faa
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-04-29 20:59:20 +0200

    Added uuid

---
+++ src/Application/Controller/Category/CreateCategoryAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Checkfood\Application\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoryController
+{
+    public function __constructor()
+    {
+
+    }
+
+    public function __invoke(Request $request)
+    {
+
+    }
+}
