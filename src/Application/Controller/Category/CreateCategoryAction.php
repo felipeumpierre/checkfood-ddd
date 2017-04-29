fd2594b9d3d0f0c271f924d71f616eefee5980ed
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-04-29 20:59:46 +0200

    Updated controller and added on application service provider

--- src/Application/Controller/Category/CreateCategoryAction.php
+++ src/Application/Controller/Category/CreateCategoryAction.php
@@ -2,17 +2,43 @@
 
 namespace Checkfood\Application\Controller;
 
+use Ramsey\Uuid\Uuid;
+use League\Tactician\CommandBus;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Checkfood\Business\Command\Category\CreateCategoryCommand;
 
-class CategoryController
+class CreateCategoryAction
 {
-    public function __constructor()
-    {
+    /**
+     * @var CommandBus
+     */
+    protected $bus;
 
+    /**
+     * @param CommandBus $bus
+     */
+    public function __constructor(CommandBus $bus)
+    {
+        $this->bus = $bus;
     }
 
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
     public function __invoke(Request $request)
     {
+        $id = (string) Uuid::uuid4();
 
+        $this->bus->handle(
+            new CreateCategoryCommand(
+                $id,
+                $request->request->get('name')
+            )
+        );
+
+        return new Response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- src/Application/ServiceProvider/ApplicationServiceProvider.php
+++ src/Application/ServiceProvider/ApplicationServiceProvider.php
@@ -4,6 +4,7 @@ namespace Checkfood\Application\ServiceProvider;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Checkfood\Application\Controller\CreateCategoryAction;
 
 class ApplicationServiceProvider implements ServiceProviderInterface
 {
@@ -12,6 +13,18 @@ class ApplicationServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $container)
     {
+        $this->registerController($container);
+    }
 
+    /**
+     * @param Container $container
+     */
+    private function registerController(Container $container)
+    {
+        $container[CreateCategoryAction::class] = function (Container $container) {
+            return new CreateCategoryAction(
+                $container['bus']
+            );
+        };
     }
 }
