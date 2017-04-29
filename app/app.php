6591b7ab59972951aad08af625c5b552a3ac1afe
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-04-29 20:37:51 +0200

    Doctrine provider

--- app/app.php
+++ app/app.php
@@ -1,6 +1,18 @@
 <?php declare(strict_types=1);
 
+use Checkfood\Application\ServiceProvider\CommandBusServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
+
 $app = new Silex\Application();
+
+$app->register(new DoctrineServiceProvider(), [
+    'db.options' => [
+        'dbname' => getenv('MYSQL_DBNAME'),
+        'host' => getenv('MYSQL_HOST'),
+        'user' => getenv('MYSQL_USER'),
+        'password' => getenv('MYSQL_PASSWORD'),
+    ],
+]);
 
 $app->register(new CommandBusServiceProvider());
 
