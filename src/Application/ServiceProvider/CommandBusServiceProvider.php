ec9c0a46b7ce99326ded1179c872cc219ab3ed52
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-04-29 20:38:40 +0200

    Initialize the command bus and other dependencies

--- src/Application/ServiceProvider/CommandBusServiceProvider.php
+++ src/Application/ServiceProvider/CommandBusServiceProvider.php
@@ -4,7 +4,14 @@ namespace Checkfood\Application\ServiceProvider;
 
 use Bezdomni\Tactician\Pimple\PimpleLocator;
 use Checkfood\Business\Command\Category\CreateCategoryCommand;
+use Checkfood\Business\Handler\Category\CreateCategoryHandler;
+use Checkfood\Domain\Repository\CategoryRepositoryInterface;
+use Checkfood\Infrastructure\Tactician\DbalTransactionMiddleware;
 use League\Tactician\CommandBus;
+use League\Tactician\Handler\CommandHandlerMiddleware;
+use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
+use League\Tactician\Handler\MethodNameInflector\HandleInflector;
+use League\Tactician\Plugins\LockingMiddleware;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
@@ -15,8 +22,28 @@ class CommandBusServiceProvider implements ServiceProviderInterface
      */
     public function register(Container $container)
     {
+        $this->registerCommandBusMiddleware($container);
         $this->registerCommandBus($container);
         $this->registerLocator($container);
+        $this->registerHandlers($container);
+    }
+
+    /**
+     * @param Container $container
+     */
+    private function registerCommandBusMiddleware(Container $container)
+    {
+        $container['bus.middleware'] = function (Container $container) {
+            return [
+                new LockingMiddleware(),
+                new DbalTransactionMiddleware($container['db']),
+                new CommandHandlerMiddleware(
+                    new ClassNameExtractor(),
+                    $container['bus.locator'],
+                    new HandleInflector()
+                ),
+            ];
+        };
     }
 
     /**
@@ -26,25 +53,35 @@ class CommandBusServiceProvider implements ServiceProviderInterface
      */
     private function registerCommandBus(Container $container)
     {
-        $container['bus'] = function () {
-            return new CommandBus([]);
+        $container['bus'] = function (Container $container) {
+            return new CommandBus($container['bus.middleware']);
         };
     }
 
+    /**
+     * @param Container $container
+     */
     private function registerLocator(Container $container)
     {
         $container['bus.locator'] = function (Container $container) {
             return new PimpleLocator(
                 $container,
                 [
-                    CreateCategoryCommand::class => CreateCategoryCommand::class,
+                    CreateCategoryCommand::class => CreateCategoryHandler::class,
                 ]
             );
         };
     }
 
+    /**
+     * @param Container $container
+     */
     private function registerHandlers(Container $container)
     {
-
+        $container[CreateCategoryHandler::class] = function (Container $container) {
+            return new CreateCategoryHandler(
+                $container[CategoryRepositoryInterface::class]
+            );
+        };
     }
 }
