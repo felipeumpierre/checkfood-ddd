c9eba68005a2b520c1904c9057ba2d4188e7ac04
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-05-01 22:43:38 +0200

    Updates

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/app.php";
+$app = require_once __DIR__ . "/app.php";
+require_once __DIR__ . "/config.php";
 require_once __DIR__ . "/routes.php";
+
+return $app;
