17b06503859f97a5bf91c3b232a30e2cd5a23614
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-05-01 22:41:20 +0200

    Folder for migrations

---
+++ src/Infrastructure/DoctrineMigrations/Version20170501161651.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Checkfood\Infrastructure\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170501161651 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('category');
+        $table->addColumn('category_id', Type::INTEGER, ['unsigned' => true, 'autoincrement' => true]);
+        $table->addColumn('name', Type::STRING, ['length' => 100]);
+
+        $table->setPrimaryKey(['category_id']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('category');
+    }
+}
