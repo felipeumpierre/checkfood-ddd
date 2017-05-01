597e67b4598c582d94fc2886608aa2d672c8d241
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-05-01 22:43:51 +0200

    Update routes

--- app/routes.php
+++ app/routes.php
@@ -3,5 +3,14 @@
 /** @var \Silex\Application $app */
 
 use Checkfood\Application\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
-$app->post('/category/create', Controller\CreateCategoryAction::class);
+$app->get('/', function () {
+    return new Response('All good!', Response::HTTP_OK);
+});
+
+$app->mount('/category', function (\Silex\ControllerCollection $category) {
+    $category->get('/{id}', Controller\Category\ListCategoryAction::class)
+        ->assert('id', '\d+');
+    $category->post('/create', Controller\Category\CreateCategoryAction::class);
+});
