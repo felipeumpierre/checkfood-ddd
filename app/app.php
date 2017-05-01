134e3e57bbab29c363823f642b01e655f477678b
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-05-01 22:43:27 +0200

    New service providers

--- app/app.php
+++ app/app.php
@@ -1,19 +1,21 @@
 <?php declare(strict_types=1);
 
+use Checkfood\Application\ServiceProvider\ApplicationServiceProvider;
 use Checkfood\Application\ServiceProvider\CommandBusServiceProvider;
+use Checkfood\Application\ServiceProvider\InfrastructureServiceProvider;
+use Checkfood\Application\ServiceProvider\MigrationDoctrineServiceProvider;
 use Silex\Provider\DoctrineServiceProvider;
+use WhoopsPimple\WhoopsServiceProvider;
 
 $app = new Silex\Application();
 
-$app->register(new DoctrineServiceProvider(), [
-    'db.options' => [
-        'dbname' => getenv('MYSQL_DBNAME'),
-        'host' => getenv('MYSQL_HOST'),
-        'user' => getenv('MYSQL_USER'),
-        'password' => getenv('MYSQL_PASSWORD'),
-    ],
-]);
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
 
+$app->register(new WhoopsServiceProvider());
+$app->register(new DoctrineServiceProvider());
+$app->register(new MigrationDoctrineServiceProvider());
+$app->register(new ApplicationServiceProvider());
 $app->register(new CommandBusServiceProvider());
+$app->register(new InfrastructureServiceProvider());
 
 return $app;
