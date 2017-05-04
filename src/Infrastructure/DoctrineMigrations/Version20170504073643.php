706b62f1b44201009b85ca83b88a34bcf39df1b4
Felipe Pieretti Umpierre <fum@hellofresh.com>
2017-05-04 09:58:44 +0200

    Migration for insert default values on category table

---
+++ src/Infrastructure/DoctrineMigrations/Version20170504073643.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Checkfood\Infrastructure\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170504073643 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->connection->insert(
+            'category',
+            [
+                'name' => 'Test 1'
+            ]
+        );
+
+        $this->connection->insert(
+            'category',
+            [
+                'name' => 'Test 2'
+            ]
+        );
+
+        $this->connection->insert(
+            'category',
+            [
+                'name' => 'Test 3'
+            ]
+        );
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->connection->delete('category', ['id' => 1]);
+        $this->connection->delete('category', ['id' => 2]);
+        $this->connection->delete('category', ['id' => 3]);
+    }
+}
